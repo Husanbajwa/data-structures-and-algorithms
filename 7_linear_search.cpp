21e8553ff7bd92bd4072d76e9a8f01b46daf5057
Husanbajwa <76039437+Husanbajwa@users.noreply.github.com>
2022-05-28 22:29:58 +0530

    algo for linear search

---
+++ 7_linear_search.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[5];
+    int loc;
+    int pos;
+    int n;
+    cout<<" Enter 5 Elements For List ";
+    for (int i = 0; i < 5; i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"EnterThe element you want to find : ";
+    cin>>n;
+    for (int i = 0; i < 5; i++)
+    {loc=NULL;
+        if (n==arr[i]){
+            loc=i+1;
+            break;
+        }
+    }
+    if (loc==NULL)
+    {
+        cout<<"OOPS no element fonnd list";
+    }
+    else{
+        cout<<"Element found at position " <<loc<<endl;
+    }
+    return 0;
+}
